84d9578f53e146572cc90d1c128f3dab5f79d20c
Pavel Baksheev <sxeiter@mail.ru>
2025-11-15 19:56:21 +0500

    подключил бд и настроил модель

---
+++ internal/link/model.go
@@ -0,0 +1,30 @@
+package link
+
+import (
+	"math/rand/v2"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: RandStringRunes(10),
+	}
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	return string(b)
+}
